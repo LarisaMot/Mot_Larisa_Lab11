6d2a154a1c41320fa527c9dc5a4956278c9e7bd0
larisa <larisa.mot@stud.ubbcluj.ro>
2025-12-16 17:50:51 +0200

    Lab11

---
+++ Mot_Larisa_Lab11/App.xaml.cs
@@ -0,0 +1,15 @@
+﻿using Mot_Larisa_Lab11.Data;
+
+namespace Mot_Larisa_Lab11
+{
+    public partial class App : Application
+    {
+        public static ShoppingListDatabase Database { get; private set; }
+        public App()
+        {
+            InitializeComponent();
+            Database = new ShoppingListDatabase(new RestService());
+            MainPage = new NavigationPage(new ListEntryPage());
+        }
+    }
+}
---
+++ Mot_Larisa_Lab11/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace Mot_Larisa_Lab11
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
